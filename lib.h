google/neper

commit 253d3b405e83fda739483d9995590647225500a7

neper: collect stats in *rr servers (#22)
We collect stats in *rr clients because we have a good measure of
round-trip latency.
For *rr servers, the latency is meaningless and is always 0.0,
but other stats might be useful.

rr.c
@@ -124,9 +124,6 @@ static struct neper_stat *rr_latency_init(struct flow *f)
         const struct thread *t = flow_thread(f);
         int size;
 
-        if (!t->opts->client)
-                return NULL;
-
         struct neper_histo *histo = t->histo_factory->create(t->histo_factory);
 
         size = sizeof(struct rr_snap_opaque) + t->percentiles * sizeof(double);
@@ -387,9 +384,16 @@ static void crr_client_state_0(struct flow *f, uint32_t events)
 static void rr_server_state_2(struct flow *f, uint32_t events)
 {
         struct rr_state *rr = flow_opaque(f);
+        struct thread *t = flow_thread(f);
+        struct neper_stat *stat = flow_stat(f);
+        struct neper_histo *histo = stat->histo(stat);
 
-        if (rr_do_send(f, events, rr->rr_send))
+        if (rr_do_send(f, events, rr->rr_send)) {
+                /* rr server has no meaningful latency to measure. */
+                histo->event(histo, 0.0);
+                stat->event(t, stat, 1, false, rr_snapshot);
                 flow_mod(f, rr_server_state_0, EPOLLIN, false);
+        }
 }
 
 static void rr_server_state_1(struct flow *f)
@@ -485,9 +489,6 @@ int rr_report_stats(struct thread *tinfo)
         struct callbacks *cb = tinfo[0].cb;
         FILE *csv = NULL;
         int i;
-
-        if (!opts->client)
-                return 0;
 
         int num_events = thread_stats_events(tinfo);
         PRINT(cb, "num_transactions", "%d", num_events);

commit 6d0f6599688ede3b8e31f0cec99a14ef5369b96a

neper: don't collect latency stats in tcp_crr server
For tcp_crr server, a new flow is created for each short-lived
connection.  If we collect stats, flow's neper_stat will be added
to worker thread's neper_stats, and won't be freed until the worker
thread ends.  It will use a lot of memory (~1GB/sec) and cause test
failures.

For other four workloads including tcp_crr client, the number of
flows is specified by command line flag, so the memory useage is
stable.

Fixes: 253d3b405 ("neper: collect stats in *rr servers")

lib.h
@@ -116,6 +116,9 @@ struct options {
         int request_size;
         int response_size;
         struct percentiles percentiles;
+
+        /* tcp_crr */
+        bool nostats;
 };
 
 #ifdef __cplusplus

rr.c
@@ -123,6 +123,8 @@ static struct neper_stat *rr_latency_init(struct flow *f)
 {
         const struct thread *t = flow_thread(f);
         int size;
+        if (t->opts->nostats)
+                return NULL;
 
         struct neper_histo *histo = t->histo_factory->create(t->histo_factory);
 
@@ -386,12 +388,14 @@ static void rr_server_state_2(struct flow *f, uint32_t events)
         struct rr_state *rr = flow_opaque(f);
         struct thread *t = flow_thread(f);
         struct neper_stat *stat = flow_stat(f);
-        struct neper_histo *histo = stat->histo(stat);
+        struct neper_histo *histo = stat ? stat->histo(stat) : NULL;
 
         if (rr_do_send(f, events, rr->rr_send)) {
-                /* rr server has no meaningful latency to measure. */
-                histo->event(histo, 0.0);
-                stat->event(t, stat, 1, false, rr_snapshot);
+                if (stat) {
+                        /* rr server has no meaningful latency to measure. */
+                        histo->event(histo, 0.0);
+                        stat->event(t, stat, 1, false, rr_snapshot);
+                }
                 flow_mod(f, rr_server_state_0, EPOLLIN, false);
         }
 }
@@ -489,6 +493,9 @@ int rr_report_stats(struct thread *tinfo)
         struct callbacks *cb = tinfo[0].cb;
         FILE *csv = NULL;
         int i;
+
+        if (opts->nostats)
+                return 0;
 
         int num_events = thread_stats_events(tinfo);
         PRINT(cb, "num_transactions", "%d", num_events);

tcp_crr.c
@@ -36,5 +36,8 @@ static const struct neper_fn server_fn = {
 int tcp_crr(struct options *opts, struct callbacks *cb)
 {
         const struct neper_fn *fn = opts->client ? &client_fn : &server_fn;
+        /* tcp_crr server doesn't collect stats, as it uses too much memory. */
+        if (!opts->client)
+                opts->nostats = true;
         return run_main_thread(opts, cb, fn);
 }